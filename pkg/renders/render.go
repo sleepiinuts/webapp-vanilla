6dc09cdbab3842ac695190bf4a6813479a9d03a1
sleepynut <nut.pa@kbtg.tech>
2024-05-21 22:21:15 +0700

    make renderFromMap more flexible

--- pkg/renders/render.go
+++ pkg/renders/render.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 const basePath = "../../templates/"
@@ -19,31 +20,54 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	parsedTmpl, err := template.ParseFiles(tPath, btPath)
 	if err != nil {
-		logger.Error("Unable to find template: ", "path", tPath, "error", err)
+		logger.Error("unable to find template: ", "path", tPath, "error", err)
 		return
 	}
 
 	err = parsedTmpl.Execute(w, nil)
 	if err != nil {
-		logger.Error("Unable to render template: ", "path", tPath, "error", err)
+		logger.Error("unable to render template: ", "path", tPath, "error", err)
 	}
 }
 
 func RenderTemplateFromMap(w http.ResponseWriter, tmpl string) {
 	tPath := basePath + tmpl
 
+	// cache template
 	if _, ok := tc[tmpl]; !ok {
-		parsedTmpl, err := template.ParseFiles(tPath, btPath)
+
+		// initial template
+		page, err := template.New(tmpl).ParseFiles(basePath + tmpl)
 		if err != nil {
-			logger.Error("Unable to find template: ", "path", tPath, "error", err)
+			logger.Error("template initializing: ", "error", err)
 			return
 		}
-		logger.Info("Caching template", "path", tPath)
+
+		// check if layout exist
+		pattern := "*.layout.tmpl"
+		matches, err := filepath.Glob(basePath + pattern)
+		if err != nil {
+			logger.Error("template layout gathering: ", "basePath", basePath, "pattern", pattern)
+			return
+		}
+		logger.Info("template layout matches: ", "matches", matches)
+
+		// if layout exist, include layout into "initialized template (page)"
+		var parsedTmpl *template.Template
+		if len(matches) > 0 {
+			parsedTmpl, err = page.ParseGlob(basePath + pattern)
+			if err != nil {
+				logger.Error("unable to find template: ", "path", tPath, "error", err)
+				return
+			}
+		}
+
+		logger.Info("caching template", "path", tPath)
 		tc[tmpl] = parsedTmpl
 	}
 
 	err := tc[tmpl].Execute(w, nil)
 	if err != nil {
-		logger.Error("Unable to render template: %s: %v", tPath, err)
+		logger.Error("unable to render template: ", "templateName", tmpl, "error", err)
 	}
 }
