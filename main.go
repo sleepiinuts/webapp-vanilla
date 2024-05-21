6ac2706115b9c325b487e4b7c043ea979ed3e304
sleepynut <nut.pa@kbtg.tech>
2024-05-21 11:11:47 +0700

    reorg code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -14,22 +13,4 @@ func main() {
 
 	log.Printf("Starting on port: \"%s\"\n", port)
 	http.ListenAndServe(port, nil)
-}
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "This is the home page")
-	if err != nil {
-		log.Fatalf("response writing error: %v", err)
-	}
-
-	log.Printf("Number of byte written: %d\n", n)
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "This is the about page")
-	if err != nil {
-		log.Fatalf("response writing error: %v", err)
-	}
-
-	log.Printf("Number of byte written: %d\n", n)
 }
