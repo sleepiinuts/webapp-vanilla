1abec187ab5bc3a8b9cc8af63ca12ea2610e6381
sleepynut <nut.pa@kbtg.tech>
2024-05-21 11:13:49 +0700

    separate render

--- handlers.go
+++ handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"html/template"
-	"log/slog"
 	"net/http"
 )
 
@@ -12,18 +10,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	pathTmpl := "./templates/" + tmpl
-	parsedTmpl, err := template.ParseFiles(pathTmpl)
-	if err != nil {
-		slog.Error("Unable to find template: %s: %v", pathTmpl, err)
-		return
-	}
-
-	err = parsedTmpl.Execute(w, nil)
-	if err != nil {
-		slog.Error("Unable to render template: %s: %v", pathTmpl, err)
-	}
 }
