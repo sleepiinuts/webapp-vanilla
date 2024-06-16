8ba960d614b89be8b6f4220e5926537d0cc59f2b
sleepynut <nut.pa@kbtg.tech>
2024-06-16 11:00:11 +0700

    override string method for *Room

--- pkg/models/room.go
+++ pkg/models/room.go
@@ -1,9 +1,15 @@
 package models
 
+import "fmt"
+
 type Room struct {
 	ID      int     `db:"id"`
 	Name    string  `db:"name"`
 	Desc    string  `db:"description"`
 	Price   float32 `db:"price"`
 	ImgPath string  `db:"img_path"`
+}
+
+func (r *Room) String() string {
+	return fmt.Sprintf("%+v", *r)
 }
