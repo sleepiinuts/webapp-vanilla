9efa68830850d2cdaacb5991f942bed1cbb0b3eb
sleepynut <nut.pa@kbtg.tech>
2024-06-24 17:04:49 +0700

    add admin routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,17 +3,18 @@ package routes
 import (
 	"net/http"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
 	"github.com/sleepiinuts/webapp-plain/configs"
 	"github.com/sleepiinuts/webapp-plain/internal/handlers"
 	"github.com/sleepiinuts/webapp-plain/internal/middlewares"
 )
 
-func Routes(h *handlers.Handler, ap *configs.AppProperties) http.Handler {
+func Routes(h *handlers.Handler, ap *configs.AppProperties, sm *scs.SessionManager) http.Handler {
 	mux := chi.NewRouter()
 
 	// create middleware
-	mw := middlewares.New(ap)
+	mw := middlewares.New(ap, sm)
 
 	mux.Use(mw.CSRFHandler)
 
@@ -23,9 +24,17 @@ func Routes(h *handlers.Handler, ap *configs.AppProperties) http.Handler {
 	mux.Get("/rooms", h.Rooms)
 	mux.Get("/make-reservation", h.MakeReservation)
 	mux.Get("/reservation-summary", h.ReservationSumm)
+	mux.Get("/login", h.Login)
 
 	mux.Post("/make-reservation", h.PostMakeReservation)
 	mux.Post("/check-room-avail", h.PostCheckRoomAvail)
+	mux.Post("/login", h.PostLogin)
+
+	// add admin routes
+	mux.Route("/admin", func(m chi.Router) {
+		m.Use(mw.Authen)
+		m.Get("/dashboard", h.AdminDashBoard)
+	})
 
 	// set up static file FileSystem relative to main.go
 	fs := http.FileServer(http.Dir("../web/static"))
