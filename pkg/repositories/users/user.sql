0755264e26229433412674bec85bbc246c37cafb
sleepynut <nut.pa@kbtg.tech>
2024-06-24 17:05:37 +0700

    initial work with user

--- pkg/repositories/users/user.sql
+++ pkg/repositories/users/user.sql
@@ -1,3 +1,9 @@
--- name: newUser
+-- name: new
 INSERT INTO users (first_name,last_name,email,"password" ,phone,"role",created_at,updated_at) 
-VALUES ($firstname,$lastName,$email,$password,$phone,$role,current_timestamp,current_timestamp) RETURNING id;
+VALUES ($1,$2,$3,$4,$5,$6,current_timestamp,current_timestamp) RETURNING id;
+
+-- name: authen
+SELECT id,password FROM users WHERE email=$1
+
+-- name: findByEmail
+SELECT first_name,last_name,email,"password",phone,"role" FROM users WHERE email=$1
