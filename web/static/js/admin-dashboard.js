b622748f2e1a30d8b73a23c66629c02d6b80cb2d
sleepynut <nut.pa@kbtg.tech>
2024-07-13 13:15:46 +0700

    add dynamic calendar & nav buttons

--- web/static/js/admin-dashboard.js
+++ web/static/js/admin-dashboard.js
@@ -1,16 +1,71 @@
-function focus() {
-  let el = document.getElementById("hover");
-  //   el.onmouseover = function () {
-  //     el.classList.add("hover");
-  //   };
-
-  //   el.onmouseleave = function () {
-  //     el.classList.remove("hover");
-  //   };
-  el.onclick = function () {
-    el.classList.toggle("clickedd");
-    // alert(el.classList);
-  };
+function clickk(el) {
+  el.classList.toggle("clickedd");
 }
 
-focus();
+function tabClick(el) {
+  let els = document.getElementsByClassName("tab");
+
+  Array.from(els).forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  el.classList.add("active");
+
+  // redirect to corersponding room calendar
+  // ?rid={roomId}&y={year}&m={month}
+
+  // get query params
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  // console.log("id: ", el);
+
+  const y = urlParams.get("y");
+  const m = urlParams.get("m");
+
+  window.location.href = `http://localhost:8080/admin/dashboard?rid=${el.id}&y=${y}&m=${m}`;
+}
+
+function calMonth(added, d, rid) {
+  // add/subtract 1 month
+  d.setMonth(d.getMonth() + added);
+  const ny = d.getYear() + 1900;
+  let nm = d.getMonth() + 1;
+  nm = nm.toString().padStart(2, "0");
+  window.location.href = `http://localhost:8080/admin/dashboard?rid=${rid}&y=${ny}&m=${nm}`;
+}
+
+function nextMonth() {
+  // get query params
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const rid = urlParams.get("rid");
+  const y = parseInt(urlParams.get("y"));
+  const m = parseInt(urlParams.get("m"));
+
+  const d = new Date(`${y}-${m}-01`);
+  calMonth(1, d, rid);
+}
+function prevMonth() {
+  // get query params
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const rid = urlParams.get("rid");
+  const y = parseInt(urlParams.get("y"));
+  const m = parseInt(urlParams.get("m"));
+
+  const d = new Date(`${y}-${m}-01`);
+  calMonth(-1, d, rid);
+}
+
+function currMonth() {
+  // get query params
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const rid = urlParams.get("rid");
+  const d = new Date();
+  calMonth(0, d, rid);
+}
